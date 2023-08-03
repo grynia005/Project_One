e56e98c82d9723615478bc16300efdf3367f77f9
grynia005 <123405339+grynia005@users.noreply.github.com>
2023-08-03 16:47:23 +0300

    'corected'

--- js/pictures.js
+++ js/pictures.js
@@ -1,23 +1,21 @@
 import { fotos } from './main.js'
-console.log(fotos)
+
 
 const photoTemplate = document.querySelector('#picture');
 const sectionPictures = document.querySelector('.pictures');
-console.log(sectionPictures.tagName)
 const allPhotoFragment = document.createDocumentFragment();
 
 function getOnePhoto(el) {
     const copyTemplate = photoTemplate.content.cloneNode(true);
-    const img = copyTemplate.querySelector('.picture__img');
-    img.src = el.url;
-    const numberOfComments = copyTemplate.querySelector('.picture__comments');
-    const numberOfLikes = copyTemplate.querySelector('.picture__likes');
-    numberOfComments.textContent = el.comments.length;
-    numberOfLikes.textContent = el.likes;
+    copyTemplate.querySelector('.picture__img').src = el.url;
+    copyTemplate.querySelector('.picture__comments').textContent = el.comments.length;
+    copyTemplate.querySelector('.picture__likes').textContent = el.likes;
     return copyTemplate;
 }
+
 function getAllPhoto() {
     fotos.forEach((el) => allPhotoFragment.appendChild(getOnePhoto(el)));
 }
+
 getAllPhoto()
 sectionPictures.append(allPhotoFragment);
