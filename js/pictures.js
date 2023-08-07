c64dfcca895cf2b726353026f51abaaa5aa84eef
grynia005 <123405339+grynia005@users.noreply.github.com>
2023-08-07 17:04:08 +0300

    'fullScreen'

--- js/pictures.js
+++ js/pictures.js
@@ -1,23 +1,25 @@
 import { fotos } from './main.js'
-console.log(fotos)
+// console.log(fotos)
 
 const photoTemplate = document.querySelector('#picture');
 const sectionPictures = document.querySelector('.pictures');
-console.log(sectionPictures.tagName)
 const allPhotoFragment = document.createDocumentFragment();
 
 function getOnePhoto(el) {
     const copyTemplate = photoTemplate.content.cloneNode(true);
-    const img = copyTemplate.querySelector('.picture__img');
-    img.src = el.url;
-    const numberOfComments = copyTemplate.querySelector('.picture__comments');
-    const numberOfLikes = copyTemplate.querySelector('.picture__likes');
-    numberOfComments.textContent = el.comments.length;
-    numberOfLikes.textContent = el.likes;
+    copyTemplate.querySelector('.picture__img').src = el.url;;
+    copyTemplate.querySelector('.picture__comments').textContent = el.comments.length;
+    copyTemplate.querySelector('.picture__likes').textContent = el.likes;
+    copyTemplate.querySelector('.picture').setAttribute('data-id', el.id)
     return copyTemplate;
 }
 function getAllPhoto() {
     fotos.forEach((el) => allPhotoFragment.appendChild(getOnePhoto(el)));
 }
 getAllPhoto()
-sectionPictures.append(allPhotoFragment);
+function addFragmentToTheContainer(container, fragment) {
+    container.appendChild(fragment)
+}
+addFragmentToTheContainer(sectionPictures, allPhotoFragment);
+export {addFragmentToTheContainer}
+// export {sectionPictures}
