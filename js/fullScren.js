dedc9419ba553456fba99bc8f2641cd486d8c7a0
grynia005 <123405339+grynia005@users.noreply.github.com>
2023-08-21 22:18:15 +0300

    'Coments_done'

--- js/fullScren.js
+++ js/fullScren.js
@@ -1,92 +1,124 @@
-import { fotos } from './main.js'
-import { addFragmentToTheContainer } from './pictures.js'
-// import { sectionPictures } from './pictures.js'
+import { photos } from "./main.js";
+import { addFragmentToTheContainer } from "./pictures.js";
+console.log(photos);
 
-const sectionPictures = document.querySelector('.pictures')
-const sectionBigPicture = document.querySelector('.big-picture');
-const bigPictureSocial = sectionBigPicture.querySelector('.big-picture__social')
-const bigIMG = sectionBigPicture.querySelector('.big-picture__img');
-const socialHeader = sectionBigPicture.querySelector('.social__header')
-const comentTemplate = document.querySelector('#social__comment')
-const listOfComents = sectionBigPicture.querySelector('.social__comments')  
-const fragmentListComment = document.createDocumentFragment()
-const buttonCancel = sectionBigPicture.querySelector('.big-picture__cancel')
-const body = document.querySelector('body')
-export {body}
+const sectionPictures = document.querySelector(".pictures");
+const sectionBigPicture = document.querySelector(".big-picture");
+const bigPictureSocial = sectionBigPicture.querySelector(
+  ".big-picture__social"
+);
+const bigIMG = sectionBigPicture.querySelector(".big-picture__img");
+const socialHeader = sectionBigPicture.querySelector(".social__header");
+const comentTemplate = document.querySelector("#social__comment");
+const listOfComents = sectionBigPicture.querySelector(".social__comments");
+const fragmentListComment = document.createDocumentFragment();
+const buttonCancel = sectionBigPicture.querySelector(".big-picture__cancel");
+const body = document.querySelector("body");
+const buttonLoadMore = sectionBigPicture.querySelector(
+  ".social__comments-loader"
+);
+const totalNumberComments = sectionBigPicture.querySelector(".comments-count");
+const displayedNumberComments =
+  sectionBigPicture.querySelector(".comments-shown");
 
+let numberOfOutputComments = 5;
+const stepComments = 5;
 
-sectionPictures.addEventListener('click', (evt) => {
-    const targetElement = evt.target;  
-    const liComent = listOfComents.querySelectorAll('LI')
-    for(let i = 0; i < liComent.length; i++) {
-        liComent[i].remove()
-    }
-    body.classList.add('modal-open')
-    const linkClick =  targetElement.closest('[data-id]')
-    const dataId = Number(linkClick.getAttribute('data-id'))
-    const photo = fotos.find((photo) => photo.id === dataId)
-    if(photo) { 
-        sectionBigPicture.classList.remove('hidden')
-        const arrComents = JSON.parse(JSON.stringify(photo.comments));
-        createListComents(arrComents, fragmentListComment, createLiComent)        
-        createFullSizePhoto(photo)
-        addFragmentToTheContainer(listOfComents, fragmentListComment);
-    }
-});
+let currentComments = [];
 
+function clearCommentList() {
+  listOfComents.innerHTML = "";
+}
+
+function hideAddCommentsButton() {
+  if (displayedNumberComments.textContent === totalNumberComments.textContent) {
+    buttonLoadMore.classList.add("hidden");
+  }
+}
+
+function loadComments() {
+  numberOfOutputComments = addComent(numberOfOutputComments);
+  clearCommentList();
+  const arrComents = JSON.parse(JSON.stringify(currentComments));
+  const displayedComents = checkingTheNumberOfComments(arrComents);
+  displayedNumberComments.textContent = displayedComents.length;
+  createListComents(displayedComents, fragmentListComment, createLiComent);
+  addFragmentToTheContainer(listOfComents, fragmentListComment);
+  hideAddCommentsButton()
+}
+
+function addComent(numb) {
+  return (numb += stepComments);
+}
 
 function createFullSizePhoto(photo) {
-    bigIMG.querySelector('img').src = photo.url
-    // console.log(photo.url)
-    bigIMG.querySelector('img').setAttribute('alt', photo.decription)
-    socialHeader.querySelector('img').src = photo.url // тут може бути Ваша фотографія))
-    socialHeader.querySelector('.social__caption').textContent = photo.decription
-    socialHeader.querySelector('.likes-count').textContent = photo.likes
+  bigIMG.querySelector("img").src = photo.url;
+  bigIMG.querySelector("img").setAttribute("alt", photo.decription);
+  socialHeader.querySelector("img").src = photo.url; // тут може бути Ваша фотографія))
+  socialHeader.querySelector(".social__caption").textContent = photo.decription;
+  socialHeader.querySelector(".likes-count").textContent = photo.likes;
 }
 
 function createLiComent(el) {
-    const copyTemplate = comentTemplate.content.cloneNode(true);
-    copyTemplate.querySelector('.social__picture').src = el.avatar
-    copyTemplate.querySelector('.social__author').textContent = el.name
-    copyTemplate.querySelector('.social__text').textContent = el.message
-    return copyTemplate
+  const copyTemplate = comentTemplate.content.cloneNode(true);
+  copyTemplate.querySelector(".social__picture").src = el.avatar;
+  copyTemplate.querySelector(".social__author").textContent = el.name;
+  copyTemplate.querySelector(".social__text").textContent = el.message;
+  return copyTemplate;
+}
 
-}
-function createListComents(array, fragment, callback) {
-    const coments = checkingTheNumberOfComments(array)
-    coments.forEach((el) => fragment.appendChild(callback(el)));
-}
 function checkingTheNumberOfComments(array) {
-    const numberOfOutputComments = 5;
-    array.splice(numberOfOutputComments, array.length - 5)
-    return array 
-};
+  array.splice(numberOfOutputComments, array.length - 5);
+  return array;
+}
+
+function createListComents(array, fragment, callback) {
+  const coments = checkingTheNumberOfComments(array);
+  coments.forEach((el) => fragment.appendChild(callback(el)));
+}
 
 function closeFullscreenPhoto() {
-    if(!sectionBigPicture.classList.contains('hidden')) {
-        sectionBigPicture.classList.add('hidden')
-        body.classList.remove('modal-open')
-    } 
-};
+  buttonLoadMore.removeEventListener("click", loadComments);
+  buttonLoadMore.classList.remove("hidden");
+  numberOfOutputComments = 5;
+  clearCommentList();
+  if (!sectionBigPicture.classList.contains("hidden")) {
+    sectionBigPicture.classList.add("hidden");
+    body.classList.remove("modal-open");
+  }
+}
 
-buttonCancel.addEventListener('click', closeFullscreenPhoto)
-document.addEventListener('keydown', (evt) => {
-    console.log(evt.key)
-    if(evt.key === 'Escape') {
-        closeFullscreenPhoto()
+sectionPictures.addEventListener("click", async (evt) => {
+  try {
+    const array = await photos;
+    const targetElement = evt.target;
+    body.classList.add("modal-open");
+    const linkClick = targetElement.closest("[data-id]");
+    const dataId = Number(linkClick.getAttribute("data-id"));
+    const photo = array.find((photo) => photo.id === dataId);
+    if (photo) {
+      currentComments = photo.comments;
+      sectionBigPicture.classList.remove("hidden");
+      totalNumberComments.textContent = currentComments.length;
+      const arrComents = JSON.parse(JSON.stringify(currentComments));
+      const displayedComents = checkingTheNumberOfComments(arrComents);
+      displayedNumberComments.textContent = displayedComents.length;
+      createListComents(displayedComents, fragmentListComment, createLiComent);
+      createFullSizePhoto(photo);
+      addFragmentToTheContainer(listOfComents, fragmentListComment);
+      buttonLoadMore.addEventListener("click", loadComments);
     }
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-// Цікавинка))
-// document.addEventListener('click', (evt) => {
-//     const containerBigPictures = document.querySelector('.big-picture__preview');
-//     // console.log(containerBigPictures.classList)
-//     console.log(evt.target.classList)
+buttonCancel.addEventListener("click", closeFullscreenPhoto);
 
-//     if (!containerBigPictures.contains(evt.target) && sectionBigPicture.classList.contains('hidden')) {
-    
-//         sectionBigPicture.classList.add('hidden');
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    closeFullscreenPhoto();
+  }
+});
 
-//         document.body.classList.remove('modal-open');
-//     }
-// });
+export { body };
